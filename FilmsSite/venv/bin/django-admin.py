092cbb27ee7630e481dfed23fb9a3a879da1360b
driver220v <driver220v@gmail.com>
2021-01-25 10:48:57 +0300

    test

---
+++ FilmsSite/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/driver220/PycharmProjects/Django_Films/Django_films/FilmsSite/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
